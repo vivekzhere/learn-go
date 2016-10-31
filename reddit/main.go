2f27175ed809af68ab97f21d058f1d2dab1f4f0c
Vivek Anand T Kallampally <vivek.anand.t.kallampally@sap.com>
2016-10-31 11:33:02 +0530

    Commit hello and make reddit a function

--- reddit/main.go
+++ reddit/main.go
@@ -5,11 +5,15 @@ import (
 	"net/http"
 	"encoding/json"
 	"fmt"
+	"errors"
 )
 
 type Item struct {
 	Title	string
 	URL	string
+}
+func (i Item) String() string {
+	return fmt.Sprintf("%s\n%s", i.Title, i.URL)
 }
 
 type Response struct {
@@ -21,22 +25,38 @@ type Response struct {
 }
 
 func main() {
-	resp, err := http.Get("http://reddit.com/r/golang.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if resp.StatusCode != http.StatusOK {
-		log.Fatal(resp.Status)
-	}
-
-	r := new(Response)
-	err = json.NewDecoder(resp.Body).Decode(r)
+	items, err := Get("golang")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, child := range r.Data.Children {
-		fmt.Println(child.Data.Title)
+	for _, item := range items {
+		fmt.Println(item)
 	}
 }
 
+func Get(reddit string) ([]Item, error) {
+	url := fmt.Sprintf("http://reddit.com/r/%s.json", reddit)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+	
+	r := new(Response)
+	err = json.NewDecoder(resp.Body).Decode(r)
+	if err != nil {
+		return nil, err
+	}
+
+	items := make([]Item, len(r.Data.Children))
+
+	for i, child := range r.Data.Children {
+		items[i] = child.Data
+	}
+	return items, nil
+}
